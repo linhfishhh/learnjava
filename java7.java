e7696005e03a13b56734be4c176229909ccb14e6
linhfishhh <linhfish2309@gmail.com>
2021-06-07 15:56:14 +0700

    s

---
+++ java7.java
@@ -0,0 +1,14 @@
+package hocjava;
+import java.util.Scanner;
+public class java7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter your name: ");
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		System.out.print("Hello: "+s);
+
+	}
+
+}
